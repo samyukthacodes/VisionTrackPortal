ec82f90f4d4e12742b4debf886f5f8c13f3cdc2b
samyukthacodes <24494611+samyukthacodes@users.noreply.github.com>
2023-07-18 14:22:33 +0530

    updatedcss

--- passport-config.js
+++ passport-config.js
@@ -1,19 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 
+const { Deta } = require('deta'); // import Deta
 
+// Initialize
+const deta = Deta(process.env.DETA_KEY);
+// This how to connect to or create a database.
+const db = deta.Base('attendance')
 
-const LocalStrategy = required('passport-local').Strategy
-
-function initializePassport(passport){
-    const authenticateUser = (username, password, done) => {
-        user = db.get(username)
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+function initialize(passport){
+    const authenticateUser = async (username, password, done) => {
+        const user = await db.get(username)
         if(user == null){
+            console.log('No user found')
             return done(null, false, {message : "Username not found."})
         }
         try{
-            if(password == user.password){
+            console.log(user.password.slice(2, -1))
+            if (await bcrypt.compare(password, user.password.slice(2, -1))){
+                console.log('Logged in Successfully')
+                console.log(user)
                 return done(null, user)
             }
             else{
+                console.log('Incorrect password')
                 return done(null, false, {message: "Incorrect Password."})
             }
         }
@@ -21,7 +34,15 @@ function initializePassport(passport){
             return done(e)
         }
     }
-    passport.use(new LocalStrategy( {usernameField: 'username'}), authenticateUser)
-    passport.serializeUser((user, done) => {})
-    passport.deserializeUser((id, done) => {})
+    passport.use(new LocalStrategy({ usernameField: 'username' }, authenticateUser))
+    passport.serializeUser((user, done) =>  done(null, user.key))
+    passport.deserializeUser(async (key, done) => {
+        try {
+          const user = await db.get(key);
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
+      });
 }
+module.exports = initialize
--- server.js
+++ server.js
@@ -4,43 +4,69 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
-    
+
 
 const { Deta } = require('deta'); // import Deta
 
 // Initialize
 const deta = Deta(process.env.DETA_KEY);
 // This how to connect to or create a database.
-const db = deta.Base('attendance');
-const initializePassport = require('./passport-config')
-initializePassport(passport)
+const db = deta.Base('attendance')
 
-const initializePassport = require('./passport-config');
-const passport = require("passport");
-initializePassport(passport)
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride= require('method-override')
+const initializePassport = require('./passport-config')
+
+initializePassport(passport, key => db.get(key))
 
 app.set('view-engine','ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(flash())
+app.use(express.static(__dirname+'/public'));
+
 app.use(session({secret:process.env.SESSION_SECRET,
     resave: false,
     saveIninitialized: false}))
 app.use(passport.initialize())
 app.use(passport.session())
-
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-    console.log(item)
+app.use(methodOverride('_method'))
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
 })
-app.post('/login', passport.authenticate('local',{
-    successRedirect: '/home',
-    failureRedirect: '/',
+app.post('/login', checkNotAuthenticated, passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/login',
     failureFlash: true
 }))
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-    console.log(item)
-})
+app.get('/',checkAuthenticated, (req, res) => {
+    console.log(req.user.name)
+    console.log(req.user.present_dates)
+    res.render('index.ejs', {name: req.user.name, datesPresent: req.user.present_dates})
+    
+})  
+app.delete('/logout', (req, res) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+  })
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
 
 app.listen(3000)
